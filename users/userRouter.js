bdef722c3d60e384f17171435bc9d032da8735f5
Kristin Barr <kristinbarr@gmail.com>
2019-10-02 19:44:56 -0400

    added DELETE request, validateUser middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,8 +8,8 @@ router.use(':/id', validateUserId)
 // POST -
 router.post('/', (req, res) => {})
 
-// POST - add a new post
-router.post('/:id/posts', (req, res) => {})
+// POST - add a new user
+router.post('/:id/posts', validateUser, (req, res) => {})
 
 // GET - all posts
 router.get('/', async (req, res) => {
@@ -49,23 +49,45 @@ router.get('/:id/posts', async (req, res) => {
   }
 })
 
-router.delete('/:id', (req, res) => {})
+// DELETE
+router.delete('/:id', async (req, res) => {
+  try {
+    await userDb.remove(req.params.id)
+    res.status(204)
+  } catch (error) {
+    // log error to server
+    console.log(error)
+    res.status(500).json({ message: 'Problem deletin user' })
+  }
+})
 
-router.put('/:id', (req, res) => {})
+// PUT
+router.put('/:id', validateUser, (req, res) => {
+
+})
 
 //custom middleware
 function validateUserId(req, res, next) {
   if (req.params.id) {
     req.user = req.user
+    next()
   } else {
-    next(res.status(400).json({message: "invalid user id"}))
+    res.status(400).json({message: "invalid user id"})
   }
 }
 
 function validateUser(req, res, next) {
-
+  if (!req.body) {
+    res.status(400).json({ message: 'missing user data' })
+  } else if (!req.name) {
+    res.status(400).json({ message: 'missing required name field' })
+  } else {
+    next()
+  }
 }
 
-function validatePost(req, res, next) {}
+function validatePost(req, res, next) {
+
+}
 
 module.exports = router
