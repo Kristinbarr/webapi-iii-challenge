f8255fc48a46315d3032d73d6d1ab67c090eab1c
Kristin Barr <kristinbarr@gmail.com>
2019-10-02 19:18:06 -0400

    added get routes, validateUserId middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,71 @@
-const express = 'express';
+const express = require('express')
+const userDb = require('../users/userDb')
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/', (req, res) => {
+router.use(':/id', validateUserId)
 
-});
+// POST -
+router.post('/', (req, res) => {})
 
-router.post('/:id/posts', (req, res) => {
+// POST - add a new post
+router.post('/:id/posts', (req, res) => {})
 
-});
+// GET - all posts
+router.get('/', async (req, res) => {
+  try {
+    const users = await userDb.get()
+    res.status(200).json(users)
+  } catch (error) {
+    // log error to server
+    console.log(error)
+    res.status(500).json({
+      message: 'Error retrieving the posts'
+    })
+  }
+})
 
-router.get('/', (req, res) => {
+// GET - return post by id
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await userDb.getById(req.params.id)
+    res.status(200).json(user)
+  } catch (error) {
+    // log error to server
+    console.log(error)
+    res.status(500).json({ message: 'User not found' })
+  }
+})
 
-});
+// GET - return posts for a user by id
+router.get('/:id/posts', async (req, res) => {
+  try {
+    const posts = await userDb.getUserPosts(req.params.id)
+    res.status(200).json(posts)
+  } catch (error) {
+    // log error to server
+    console.log(error)
+    res.status(500).json({ message: 'Posts not found' })
+  }
+})
 
-router.get('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {})
 
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {})
 
 //custom middleware
-
 function validateUserId(req, res, next) {
-
-};
+  if (req.params.id) {
+    req.user = req.user
+  } else {
+    next(res.status(400).json({message: "invalid user id"}))
+  }
+}
 
 function validateUser(req, res, next) {
 
-};
+}
 
-function validatePost(req, res, next) {
+function validatePost(req, res, next) {}
 
-};
-
-module.exports = router;
+module.exports = router
